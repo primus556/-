eef72fe76528f7bed81ef6727cd71653571fdf10
风灵git <wy1868261@163.com>
2023-02-14 17:44:26 +0800

    init project

---
+++ src/main/java/com/example/spring_brunch/SpringBrunchApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_brunch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBrunchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBrunchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring_brunch/SpringBrunchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_brunch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBrunchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
